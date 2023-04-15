6c1b517d83e8736f6a09d5d55bf5dd06392d89a0
Deepender Choudhary <deependerchoudhary2003@gmail.com>
2023-04-15 13:41:49 +0530

    first commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { add } from '../store/cartSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { add } from "../store/cartSlice";
+import { fetchProducts } from "../store/productSlice";
+import { STATUSES } from "../store/productSlice";
 
 export default function Products() {
-    const dispatch=useDispatch()
+    const dispatch = useDispatch();
+    const { data: products, status } = useSelector((state) => state.product);
+    // const [products, setProducts] = useState([]);
 
-    const [products,setProducts]=useState([])
-    useEffect(()=> {
-        const fetchData=async()=>{
-            const productData= await fetch('https://fakestoreapi.com/products');
-const data= await productData.json()
- setProducts(data)
- }
- fetchData()
-    },[])
+    useEffect(() => {
+        dispatch(fetchProducts());
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     console.log(data);
+        //     setProducts(data);
+        // };
+        // fetchProducts();
+    }, []);
 
-const handleAdd=(product)=>{
-dispatch(add(product))
-}
+    const handleAdd = (product) => {
+        dispatch(add(product));
+    };
 
+    if (status === STATUSES.LOADING) {
+        return <h2>Loading....</h2>;
+    }
 
-  return (
-    <>
-    <div className="productsWrapper">
+    if (status === STATUSES.ERROR) {
+        return <h2>Something went wrong!</h2>;
+    }
+    return (
+        <div className="productsWrapper">
             {products.map((product) => (
                 <div className="card" key={product.id}>
                     <img src={product.image} alt="" />
@@ -34,7 +44,5 @@ dispatch(add(product))
                 </div>
             ))}
         </div>
-    
-    </>
-  )
+    );
 }
